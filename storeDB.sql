d2206b03b64e0fe8cb6bd75df5250e8df536c7af
Vũ Đức <vxduc9999@gmail.com>
2020-12-07 23:10:50 +0700

    update sript database

--- storeDB.sql
+++ storeDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2020 at 05:23 AM
+-- Generation Time: Dec 07, 2020 at 05:08 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -176,8 +176,9 @@ CREATE TABLE `purchase_status` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
   `address` varchar(1000) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `password` varchar(1000) NOT NULL,
