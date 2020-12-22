9ec7d8c6f07a9c7a4455a219d3a269d98d3e43aa
= <=>
2020-12-22 19:47:58 +0700

    update detail, account, upload lovedetail

--- resources/views/Auth/detailProduct.blade.php
+++ resources/views/Auth/detailProduct.blade.php
@@ -10,67 +10,113 @@
 </head>
 
 <body>
-    <div class="container">
-
+    <div class="card-wrapper">
         <div class="card">
-            <div id="shopping">
-                <i class="fas fa-heart"><sup id="items-added"></sup></i>
-            </div>
-            <div class="images">
-                <div class="slider"><img id="big-image" src="{{ asset('client/images/product/1.jpg') }}" alt=""></div>
-                <div id="img-slider" class="img-slider">
-                    <div class="imgs"><img id="Nomos1" src="{{ asset('client/images/product/1.jpg') }}" alt=""></div>
-                    <div class="imgs"><img id="Nomos2" src="{{ asset('client/images/product/2.jpg') }}" alt=""></div>
-                    <div class="imgs"><img id="Nomos3" src="{{ asset('client/images/product/3.jpg') }}" alt=""></div>
-                </div>
-            </div>
-            <div class="infos">
-                <h1>Apple Watch Series 6</h1>
-                <div class="reviews">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                </div>
-                <div class="price">
-                    <h3>11.000.00đ</h3>
-                    <h3>13.990.000đ</h3>
-                </div>
-                <div id="more-infos">
-                    <h5 class="choose">Giới thiệu</h5>
-                    <h5 class="choose">Bảo hành</h5>
-                </div>
-                <div id="info-content">
-                    <p class="paragraph" style="display: block;">Apple Watch Series 6 44mm GPS Viền Nhôm Dây Cao Su
-                        Chính Hãng.</p>
-                    <p class="paragraph" style="display: none;">- Trong 30 ngày đầu nhập lại máy, trừ phí 20% trên giá
-                        hiện tại(hoặc giá mua nếu giá mua thấp hơn giá hiện tại)- Sau 30 ngày nhập lại máy theo giá thoả
-                        thuận</p>
-                    <p class="paragraph" style="display: none;">- Trong 30 ngày đầu nhập lại máy, trừ phí 20% trên giá
-                        hiện
-                        tại(hoặc giá mua nếu giá mua thấp hơn giá hiện tại)- Sau 30 ngày nhập lại máy theo giá thoả
-                        thuận</p>
-                </div>
-                <div class="choose-color">
-                    <h4>Chọn màu</h4>
-                    <div class="choose">
-                        <button>Màu đen</button>
-                        <button>Màu trắng</button>
-                        <button>Màu hồng</button>
+            <!-- card left -->
+            <div class="product-imgs">
+                <div class="img-display">
+                    <div class="img-showcase">
+                        <img src="{{ asset('client/images/product/1.jpg') }}" alt="shoe image">
+                        <img src="{{ asset('client/images/product/2.jpg') }}" alt="shoe image">
+                        <img src="{{ asset('client/images/product/3.jpg') }}" alt="shoe image">
+                        <img src="{{ asset('client/images/product/4.jpg') }}" alt="shoe image">
                     </div>
                 </div>
-                <div class="quantity">
-                    <h3>Số lượng</h3>
-                    <input type="number" name="items" id="counter" min="1" value="1">
+                <div class="img-select">
+                    <div class="img-item">
+                        <a href="#" data-id="1">
+                            <img src="{{ asset('client/images/product/1.jpg') }}" alt="shoe image">
+                        </a>
+                    </div>
+                    <div class="img-item">
+                        <a href="#" data-id="2">
+                            <img src="{{ asset('client/images/product/2.jpg') }}" alt="shoe image">
+                        </a>
+                    </div>
+                    <div class="img-item">
+                        <a href="#" data-id="3">
+                            <img src="{{ asset('client/images/product/3.jpg') }}" alt="shoe image">
+                        </a>
+                    </div>
+                    <div class="img-item">
+                        <a href="#" data-id="4">
+                            <img src="{{ asset('client/images/product/4.jpg') }}" alt="shoe image">
+                        </a>
+                    </div>
                 </div>
-                <div class="buttons">
-                    <button id="add-to-cart"><i class="fas fa-shopping-cart"></i>THÊM VÀO GIỎ HÀNG</button>
-                    <button>MUA NGAY</button>
+            </div>
+            <!-- card right -->
+            <div class="product-content">
+                <div class="title">
+
+                    <!-- Tên sp -->
+                    <h2 class="product-title">Apple Watch</h2>
+
+                    <!-- Icon iu thích -->
+                    <i class="fas fa-heart" style="font-size: 30px;margin: 1.4rem 0;"><sup id="items-added"></sup></i>
+                </div>
+                <a href="#" class="product-link">Series 6</a>
+                <div class="infos">
+
+                    <!-- Đánh giá sp -->
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+
+                    <!-- Giá sp -->
+                    <div class="price">
+                        <h3>11.000.00đ</h3>
+                        <h3>13.990.000đ</h3>
+                    </div>
+
+                    <!-- Thông tin sp -->
+                    <div id="more-infos">
+                        <h5 class="choose">Giới thiệu</h5>
+                        <h5 class="choose">Bảo hành</h5>
+                    </div>
+
+                    <div id="info-content">
+                        <p class="paragraph" style="display: block;">Apple Watch Series 6 44mm GPS Viền Nhôm Dây Cao Su
+                            Chính Hãng.</p>
+                        <p class="paragraph" style="display: none;">
+                            - Trong 30 ngày đầu nhập lại máy, trừ phí 20% trên giá
+                            hiện tại(hoặc giá mua nếu giá mua thấp hơn giá hiện tại)
+                            </br>
+                            - Sau 30 ngày nhập lại máy theo giá thoả
+                            thuận</p>
+                    </div>
+
+                    <!-- Chọn màu sắc sp -->
+                    <div class="choose-color">
+                        <h4>Chọn màu</h4>
+                        <div class="choose">
+                            <button>Màu đen</button>
+                            <button>Màu trắng</button>
+                            <button>Màu hồng</button>
+                        </div>
+                    </div>
+
+                    <!-- Số lượng sp -->
+                    <div class="quantity">
+                        <h3>Số lượng</h3>
+                        <input type="number" style="outline: none;" name="items" id="counter" min="1" value="1">
+                    </div>
+
+                    <!-- Ý định mua sp -->
+                    <div class="buttons">
+                        <button id="add-to-cart"><i class="fas fa-shopping-cart"></i>THÊM VÀO GIỎ HÀNG</button>
+                        <button>MUA NGAY</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Script -->
     <script src="{{ asset('client/js/product&profile.js') }}"></script>
 </body>
 
---
+++ resources/views/Auth/loveProduct.blade.php
@@ -0,0 +1,95 @@
+<div class="main_content">
+    <div class="header">
+        <h4>
+            Sản phẩm yêu thích
+        </h4>
+    </div>
+    <div class="bd-grid main">
+        <article class="card">
+            <div class="card__img">
+                <img src="avatars/1.jpg" alt="">
+            </div>
+            <div class="card__name">
+                <p>Title</p>
+            </div>
+            <div class="card__precis">
+                <a href="" class="card__icon">
+                    <i class="far fa-heart"></i>
+                </a>
+
+                <div>
+                    <span class="card__preci card__preci--before">giá gốc</span>
+                    <span class="card__preci card__preci--now">giá hiện tại</span>
+                </div>
+                <a href="" class="card__icon">
+                    <i class="fas fa-cart-plus"></i>
+                </a>
+            </div>
+        </article>
+
+        <article class="card">
+            <div class="card__img">
+                <img src="avatars/2.jpg" alt="">
+            </div>
+            <div class="card__name">
+                <p>Title</p>
+            </div>
+            <div class="card__precis">
+                <a href="" class="card__icon">
+                    <i class="far fa-heart"></i>
+                </a>
+                <div>
+                    <span class="card__preci card__preci--before">giá gốc</span>
+                    <span class="card__preci card__preci--now">giá hiện tại</span>
+                </div>
+                <a href="" class="card__icon">
+                    <i class="fas fa-cart-plus"></i>
+                </a>
+            </div>
+        </article>
+
+        <article class="card">
+            <div class="card__img">
+                <img src="avatars/3.jpg" alt="">
+            </div>
+            <div class="card__name">
+                <p>Title</p>
+            </div>
+            <div class="card__precis">
+                <a href="" class="card__icon">
+                    <i class="far fa-heart"></i>
+                </a>
+
+                <div>
+                    <span class="card__preci card__preci--before">giá gốc</span>
+                    <span class="card__preci card__preci--now">giá hiện tại</span>
+                </div>
+                <a href="" class="card__icon">
+                    <i class="fas fa-cart-plus"></i>
+                </a>
+            </div>
+        </article>
+
+        <article class="card">
+            <div class="card__img">
+                <img src="avatars/5.jpg" alt="">
+            </div>
+            <div class="card__name">
+                <p>Title</p>
+            </div>
+            <div class="card__precis">
+                <a href="" class="card__icon">
+                    <i class="far fa-heart"></i>
+                </a>
+
+                <div>
+                    <span class="card__preci card__preci--before">giá gốc</span>
+                    <span class="card__preci card__preci--now">giá hiện tại</span>
+                </div>
+                <a href="" class="card__icon">
+                    <i class="fas fa-cart-plus"></i>
+                </a>
+            </div>
+        </article>
+    </div>
+</div>
